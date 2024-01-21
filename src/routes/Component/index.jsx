fec5d53f24c9d7aacdf99e4e5f37cd480b318d07
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-01-21 01:19:03 -0800

    Get tokens for Calendar API

--- src/routes/Component/index.jsx
+++ src/routes/Component/index.jsx
@@ -1,11 +1,33 @@
-import './style.css';
+import "./style.css";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { auth } from "../../../firebase";
 
 const Component = () => {
-	return (
-		<div>
-			<h1>Component</h1>
-		</div>
-	);
+  const [queryParams, _] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        await fetch(
+          `http://127.0.0.1:5001/maomorize-b2ccb/us-central1/accessToken?uid=${
+            user.uid
+          }&code=${queryParams.get("code")}`
+        );
+        navigate("/view");
+      }
+    });
+
+    return () => unsub();
+  }, []);
+
+  return (
+    <div>
+      <h1>Component</h1>
+    </div>
+  );
 };
 
 export default Component;
