0ff84bdd999bf28acdf8c6b2204b2d4a9c931e10
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-01-20 18:02:35 -0800

    added logging in with google option

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,6 +1,10 @@
 import './style.css';
 import { useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import {
+	signInWithEmailAndPassword,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from 'firebase/auth';
 import { auth } from '../../../firebase'; // Assuming you have initialized Firebase in 'firebase.js' file
 
 const Login = () => {
@@ -17,6 +21,20 @@ const Login = () => {
 		}
 	};
 
+	const handleGoogleLogin = async () => {
+		try {
+			// Set up GoogleAuthProvider
+			const provider = new GoogleAuthProvider();
+			// Sign in with Google using a pop-up window
+			const result = await signInWithPopup(auth, provider);
+			const user = result.user;
+			console.log('User logged in with Google successfully!', user);
+			// Redirect user to View Page
+		} catch (error) {
+			console.error('Error logging in with Google:', error.message);
+		}
+	};
+
 	return (
 		<div>
 			<input
@@ -30,6 +48,7 @@ const Login = () => {
 				onChange={(e) => setPassword(e.target.value)}
 			/>
 			<button onClick={handleLogin}>Login</button>
+			<button onClick={handleGoogleLogin}>Continue with Google</button>
 		</div>
 	);
 };
