17b3e9947fa828c31729aa6e6f7caa84118ac1f4
Varun Kanna <73306137+varun-kanna@users.noreply.github.com>
2024-01-20 17:19:24 -0800

    made login work and defauly signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
-import * as React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Upload from "./routes/Upload";
-import Review from "./routes/Review";
-import View from "./routes/View";
-import Landing from "./routes/Landing";
-
+import * as React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Upload from './routes/Upload';
+import Review from './routes/Review';
+import View from './routes/View';
+import Landing from './routes/Landing';
+import SignUp from './routes/SignUp';
+import Login from './routes/Login';
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-  {
-    path: "/upload",
-    element: <Upload />,
-  },
-  {
-    path: "/view",
-    element: <View />,
-  },
-  {
-    path: "/review",
-    element: <Review />,
-  },
+	{
+		path: '/',
+		element: <Landing />,
+	},
+	{
+		path: '/upload',
+		element: <Upload />,
+	},
+	{
+		path: '/view',
+		element: <View />,
+	},
+	{
+		path: '/review',
+		element: <Review />,
+	},
+	{
+		path: '/signup',
+		element: <SignUp />,
+	},
+	{
+		path: '/login',
+		element: <Login />,
+	},
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+	return <RouterProvider router={router} />;
 };
 
 export default App;
